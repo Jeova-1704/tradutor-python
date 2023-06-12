34a7e7b90290ed1fa16c2b55a8788c3e12902396
Jeova BL <jeovab115@gmail.com>
2023-06-11 21:45:56 -0300

    Atualização do codigo e comentarios

--- main.py
+++ main.py
@@ -46,9 +46,11 @@ def copiar():
 
 
 # chamando função para centralizar janela
+
 centralizar_janela()
 
 # Divisão da tela
+
 frame_escolha = Frame(janela, width=300, height=100, bg=cor4)
 frame_escolha.grid(row=0, column=0)
 
@@ -56,17 +58,21 @@ frame_texto = Frame(janela, width=300, height=200, bg=cor1)
 frame_texto.grid(row=1, column=0)
 
 # Criando os campos de escolhas no label de frames_de_escolhas
+
 linguas = ['en', 'pt', 'fr', 'de', 'it', 'ja', 'ru', 'es']
 
 # Titulo do programa
-titulo = Label(frame_escolha, text='TRADUTO DE TEXTOS', height=20, width=20, pady=10, relief='flat', anchor=NW,
-               font='Arial 10 bold', bg=cor4, fg=cor2)
+
+titulo = Label(frame_escolha, text='TRADUTO DE TEXTOS', height=20, width=20, pady=10, relief='flat', anchor=NW, font='Arial 10 bold', bg=cor4, fg=cor2)
 titulo.place(x=80, y=0)
 
 # Primeira linha de escolha
+
 escolha_texto_de = Label(frame_escolha, text='De:', height=4, width=8, pady=7, relief="flat", anchor=NW,font='Ivy 10 bold', bg=cor4, fg=cor2)
 escolha_texto_de.place(x=50, y=40)
+
 # Criando um icone de escolhas para add valores de varios idiomas
+
 idioma_de_origem = ttk.Combobox(frame_escolha, height=4, width=4, justify=CENTER, font='Ivy 10 bold')
 idioma_de_origem['values'] = linguas
 idioma_de_origem.place(x=80, y=45)
@@ -91,12 +97,11 @@ resultado.place(x=10, y=130)
 
 # botões
 
-botao_taduzir = Button(frame_texto, text='Traduzir', command=traducao, width=5, padx=5, height=0, bg=cor1, fg=cor2,
-                       font='Ivy 12 bold', relief=RAISED, overrelief=RIDGE)
+botao_taduzir = Button(frame_texto, text='Traduzir', command=traducao, width=5, padx=5, height=0, bg=cor1, fg=cor2, font='Ivy 12 bold', relief=RAISED, overrelief=RIDGE)
 botao_taduzir.place(x=210, y=37)
-botao_copiar = Button(frame_texto, text='Copiar', command=copiar, width=5, padx=5, height=0, bg=cor1, fg=cor2,
-                      font='Ivy 12 bold', relief=RAISED, overrelief=RIDGE)
+botao_copiar = Button(frame_texto, text='Copiar', command=copiar, width=5, padx=5, height=0, bg=cor1, fg=cor2, font='Ivy 12 bold', relief=RAISED, overrelief=RIDGE)
 botao_copiar.place(x=210, y=140)
 
 # loop da janela
+
 janela.mainloop()
